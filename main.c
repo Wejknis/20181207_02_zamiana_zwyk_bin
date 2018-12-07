87019979a020dde928d8828e2fb1e25425297102
Wejknis <weja077@gmail.com>
2018-12-07 16:41:02 +0100

    początek KONIEc

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int liczba = 0;
+    int index = 0;
+    int tablica[200];
+    int i;
+
+    printf("Podaj liczbę dziesiętną: \n");
+    scanf("%d", &liczba);
+
+    do{
+        tablica[index++] = liczba % 2;
+        liczba /= 2;
+    }while (liczba > 0);
+
+    for (int i = index - 1; i >= 0; --i) {
+        printf("%d", tablica[i]);
+    }
+
     return 0;
 }
